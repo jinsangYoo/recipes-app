4b9b62c54fdbcbca5258bd934ea57f6d62533a54
jinsang <jinsang@nhndata.com>
2022-07-14 18:08:46 +0900

    dist 폴더에 js 파일과 index.html 파일 분리

--- webpack.config.js
+++ webpack.config.js
@@ -5,8 +5,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 module.exports = {
   entry: "./src/index.js",
   output: {
-    path: path.join(__dirname, "dist", "assets"),
-    filename: "bundle.js",
+    path: path.join(__dirname, "dist"),
+    filename: "assets/bundle.js",
   },
   module: {
     rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
