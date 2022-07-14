05c640ce003fd1d724da6041c5f6d8ad9612c665
jinsang <jinsang@nhndata.com>
2022-07-14 17:43:36 +0900

    레시피 앱 소스맵까지

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.join(__dirname, "dist", "assets"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }],
+  },
+  devtool: "source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "리액트 조리법 앱",
+      filename: "./indexForRecipes.html",
+      template: "index.html",
+    }),
+  ],
+}
